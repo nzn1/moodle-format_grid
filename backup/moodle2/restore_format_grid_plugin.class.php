ea4b55fd8f67f76cbc676c8991bf49f92966ac54
Gareth Barnard <1058419+gjb2048@users.noreply.github.com>
2023-08-10 13:32:02 +0100

    Work in progress - #172.

--- backup/moodle2/restore_format_grid_plugin.class.php
+++ backup/moodle2/restore_format_grid_plugin.class.php
@@ -201,10 +201,15 @@ class restore_format_grid_plugin extends restore_format_plugin {
         /* Allow this to process even if not in the grid format so that our event observer on 'course_restored'
            can perform a clean up of restored grid image files after all the data is in place in the database
            for this to happen properly. */
-
-        if ($this->step->get_task()->get_target() == backup::TARGET_NEW_COURSE) {
+        $target = $this->step->get_task()->get_target();
+        if (($target == backup::TARGET_NEW_COURSE) || ($target == backup::TARGET_CURRENT_DELETING)) {
             $courseid = $this->task->get_courseid();
             $newsectionid = $this->task->get_sectionid();
+
+            if ($target == backup::TARGET_CURRENT_DELETING) {
+                // Delete any images associated with the target course.
+                \format_grid\toolbox::delete_images($courseid);
+            }
 
             if (empty($data->contenthash)) {
                 // Less than M4.0 backup file.
