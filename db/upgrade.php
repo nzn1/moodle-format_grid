7e3888056c65f3650437f96d7b8151eb03b14657
Gareth Barnard <1058419+gjb2048@users.noreply.github.com>
2023-08-13 13:52:58 +0100

    Does not need to be so complex in M4.0 version.

--- db/upgrade.php
+++ db/upgrade.php
@@ -167,31 +167,21 @@ function xmldb_format_grid_upgrade($oldversion = 0) {
         upgrade_plugin_savepoint(true, 2022072200, 'format', 'grid');
     }
 
-    if ($oldversion < 2022112605) {
-        // Has the upgrade already happened?  Thus in previous versions for Moodle 4.2.
+    if ($oldversion < 2022072207) {
         $records = $DB->get_records('course_format_options',
             array(
                 'format' => 'grid',
-                'name' => 'gnumsections'
+                'name' => 'numsections'
             ), '', 'id'
         );
 
-        if (empty($records)) {
-            $records = $DB->get_records('course_format_options',
-                array(
-                    'format' => 'grid',
-                    'name' => 'numsections'
-                ), '', 'id'
-            );
-
-            $records = array_keys($records);
-            foreach ($records as $id) {
-                $DB->set_field('course_format_options', 'name', 'gnumsections', array('id' => $id));
-            }
+        $records = array_keys($records);
+        foreach ($records as $id) {
+            $DB->set_field('course_format_options', 'name', 'gnumsections', array('id' => $id));
         }
 
         // Grid savepoint reached.
-        upgrade_plugin_savepoint(true, 2022112605, 'format', 'grid');
+        upgrade_plugin_savepoint(true, 2022072207, 'format', 'grid');
     }
 
     // Automatic 'Purge all caches'....
